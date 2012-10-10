1913694a80294c24052e768117e39d98bc8e7395
rantes <rantes.javier@gmail.com>
2012-10-10 10:33:28 -0500

    First commit

---
+++ servidor.js
@@ -0,0 +1,10 @@
+﻿var mesclient = {message:''};
+var io = require('./socket.io/lib/socket.io').listen(8081);
+var chat = io
+  .of('/chat')
+  .on('connection', function (socket) {
+	socket.on('from client', function(data){
+		mesclient = data;
+		chat.emit('from server',{message:'Did you said: '+mesclient.message + '?'});
+	});
+  });
